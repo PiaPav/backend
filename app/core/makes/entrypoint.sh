75553d8829609b0baecc4177b043838be8e00857
sergei <vasilevsergei995@gmail.com>
2025-10-05 08:38:32 +0300

    fix cicd

--- app/core/makes/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-/src/generate_config.sh /config/config.yml /src/config/config.yml.template
-
-exec uv run python main.py
--- app/core/makes/generate_config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-CONFIG_PATH="$1"
-TEMPLATE_PATH="$2"
-
-envsubst < "$TEMPLATE_PATH" > "$CONFIG_PATH"
-
-chmod 600 "$CONFIG_PATH"
