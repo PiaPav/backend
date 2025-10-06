6fa908e2854a95801b8bc6dd506c2938e88f1b6a
sergei <vasilevsergei995@gmail.com>
2025-10-06 22:00:11 +0300

    squash: объединены последние 47 коммитов

--- app/core/src/utils/config.py
+++ app/core/src/utils/config.py
@@ -1,97 +1,55 @@
-from dataclasses import dataclass, fields, is_dataclass
-from pathlib import Path
-from typing import Any, Dict, Type, TypeVar, Optional
-
-from yaml import safe_load
-
-LOCAL_CONFIG_PATH: Path = Path(__file__).parent / "config-local.yml"
-
-T = TypeVar('T')
-
+from dataclasses import dataclass
+import os
 
 @dataclass
 class ConfigAuth:
-    """Конфиг Авторизации"""
     ACCESS_SECRET_KEY: str
     REFRESH_SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     REFRESH_TOKEN_EXPIRE_DAYS: int
 
-
 @dataclass
 class ConfigServer:
-    """Конфиг сервера"""
     host: str
     port: int
 
-
 @dataclass
 class ConfigDB:
-    """Конфиг базы данных"""
     host: str
     port: int
     name: str
     user: str
     password: str
-    echo: Optional[bool] = False
-
+    echo: bool
 
 @dataclass
 class Config:
-    """Глобальный конфиг"""
     auth: ConfigAuth
     server: ConfigServer
     db: ConfigDB
 
+def load_config() -> Config:
+    return Config(
+        auth=ConfigAuth(
+            ACCESS_SECRET_KEY=os.environ["ACCESS_SECRET_KEY"],
+            REFRESH_SECRET_KEY=os.environ["REFRESH_SECRET_KEY"],
+            ALGORITHM=os.environ.get("ALGORITHM", "HS256"),
+            ACCESS_TOKEN_EXPIRE_MINUTES=int(os.environ["ACCESS_TOKEN_EXPIRE_MINUTES"]),
+            REFRESH_TOKEN_EXPIRE_DAYS=int(os.environ["REFRESH_TOKEN_EXPIRE_DAYS"]),
+        ),
+        server=ConfigServer(
+            host=os.environ.get("CORE_HOST", "0.0.0.0"),
+            port=int(os.environ.get("CORE_PORT", 8000)),
+        ),
+        db=ConfigDB(
+            host=os.environ.get("POSTGRES_HOST", "postgres"),
+            port=int(os.environ.get("POSTGRES_PORT", 5432)),
+            name=os.environ["POSTGRES_DB"],
+            user=os.environ["POSTGRES_USER"],
+            password=os.environ["POSTGRES_PASSWORD"],
+            echo=bool(os.environ["POSTGRES_ECHO"])
+        ),
+    )
 
-class ConfigLoader:
-    """Загрузка конфига из файла"""
-    _instance = None
-
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-            cls._instance._config = cls._load_config()
-        return cls._instance
-
-    @property
-    def config(self) -> Config:
-        return self._config
-
-    @classmethod
-    def _load_config(cls) -> Config:
-        config_data = cls._read_config()
-        return cls._create_dataclass(Config, config_data)
-
-    @staticmethod
-    def _read_config() -> Dict[str, Any]:
-        if LOCAL_CONFIG_PATH.exists():
-            with open(LOCAL_CONFIG_PATH, "r") as config_file:
-                return safe_load(config_file)
-        raise FileNotFoundError('Отсутствует конфиг формата "config.yaml"')
-
-    @staticmethod
-    def _create_dataclass(cls: Type[T], data: Dict[str, Any]) -> T:
-        if not is_dataclass(cls):
-            raise ValueError(f"{cls.__name__} не является датаклассом (именем в конфигурации)")
-
-        kwargs = {}
-        for field in fields(cls):
-            field_name = field.name
-            if field_name not in data:
-                raise ValueError(f"Пропущено поле '{field_name}' в конфиге для {cls.__name__}")
-
-            field_value = data[field_name]
-            field_type = field.type
-
-            if is_dataclass(field_type):
-                kwargs[field_name] = ConfigLoader._create_dataclass(field_type, field_value)
-            else:
-                kwargs[field_name] = field_value
-
-        return cls(**kwargs)
-
-
-config_loader = ConfigLoader()
-CONFIG: Config = config_loader.config
+CONFIG = load_config()
