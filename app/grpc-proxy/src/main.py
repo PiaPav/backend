c092568a556edb5dd938c0f4acd7ffd0d23b507e
sergei <vasilevsergei995@gmail.com>
2025-12-07 20:39:56 +0300

    add ms

--- app/grpc-proxy/src/main.py
+++ app/grpc-proxy/src/main.py
@@ -1,15 +1,20 @@
 import asyncio
 from grpc_.core_server import CoreServer
+from utils.logger import create_logger
+
+log = create_logger("CoreGRPCMain")
 
 
 async def main():
     server = CoreServer(host='0.0.0.0', port=50051)
     await server.start()
+    log.info(f"gRPC CoreServer запущен на {server.port}")
+
     try:
-        while True:
-            await asyncio.sleep(3600)
+        await server.server.wait_for_termination()
     except asyncio.CancelledError:
         await server.stop()
+        log.info("gRPC CoreServer остановлен")
 
 if __name__ == "__main__":
     asyncio.run(main())
